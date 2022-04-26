cb02683c42238bed4f5160c40ca2dc11f544a210
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-04-25 20:19:56 -0400

    exercise 2 commit

---
+++ Exercise242522/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise242522
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the radius of your circle:");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine(area);
+
+            Class2.SimpleMath();
+
+
+        }
+
+        public static double AreaOfCircle(double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+
+
+    }
+}
+
